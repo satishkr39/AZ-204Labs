aa0281093decbd2b22902d9fc796926803fc2b5a
Satish Kumar Singh <60573978+satishkr39@users.noreply.github.com>
2022-10-06 09:23:31 -0700

    Adding Lab10 - Implement service bus queues

---
+++ Lab_10/MessagePublisher/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+namespace MessagePublisher
+{
+    public class Program
+    {
+        private const string storageConnectionString = "Endpoint=sb://sbnamespacesatish.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=5NN3/yybHGX4hiZMl8ivN/0IQZvZHuBFnMjeyV8L3kc=";
+        private const string queueName = "messagequeue";
+        private const int numOfMessages = 3;
+        static ServiceBusClient client = default!;
+        static ServiceBusSender sender = default!;
+        public static async Task Main(string[] args)
+        {
+            client = new ServiceBusClient(storageConnectionString);
+            sender = client.CreateSender(queueName);  
+            using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+            for (int i = 1; i <= numOfMessages; i++)
+            {
+                if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+                {
+                    throw new Exception($"The message {i} is too large to fit in the batch.");
+                }
+            }
+            try
+            {
+                await sender.SendMessagesAsync(messageBatch);
+                Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
+            }
+            finally
+            {
+                await sender.DisposeAsync();
+                await client.DisposeAsync();
+            }
+        }
+    }
+}
---
+++ Lab_10/MessageReader/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;  
+namespace MessageReader
+{
+   public class Program
+   {
+      private const string storageConnectionString = "Endpoint=sb://sbnamespacesatish.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=5NN3/yybHGX4hiZMl8ivN/0IQZvZHuBFnMjeyV8L3kc=";
+      static string queueName = "messagequeue";
+      static ServiceBusClient client = default!;
+
+      static ServiceBusProcessor processor = default!;
+      static async Task MessageHandler(ProcessMessageEventArgs args)
+        {
+            string body = args.Message.Body.ToString();
+            Console.WriteLine($"Received: {body}");
+            await args.CompleteMessageAsync(args.Message);
+        }
+        
+        static Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+
+        static async Task Main(string[] args)
+        {
+            client = new ServiceBusClient(storageConnectionString);
+            processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+            try
+            {
+                processor.ProcessMessageAsync += MessageHandler;
+                processor.ProcessErrorAsync += ErrorHandler;
+                await processor.StartProcessingAsync();
+                Console.WriteLine("Wait for a minute and then press any key to end the processing");
+                Console.ReadKey();
+                Console.WriteLine("\nStopping the receiver...");
+                await processor.StopProcessingAsync();
+                Console.WriteLine("Stopped receiving messages");
+            }
+            finally
+            {
+                await processor.DisposeAsync();
+                await client.DisposeAsync();
+            }
+
+            
+        }
+   }
+}
